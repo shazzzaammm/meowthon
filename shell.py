d16b29026f65c6338530fe68d00186e392d2efb2
K <Redknclarke@gmail.com>
2023-12-19 23:00:49 -0500

    added local files

---
+++ shell.py
@@ -0,0 +1,15 @@
+import meowthon
+
+while True:
+	text = input('∩｡• ᵕ •｡∩ ♡ > ')
+	if text.strip() == "": continue
+	result, error = meowthon.run('<stdin>', text)
+
+	print()
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
